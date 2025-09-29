8c46f36b50fe4203d88a7b248bfdeef9d18ce061
Franz Naling <88300093+franz-naling@users.noreply.github.com>
2025-09-29 18:45:17 +1300

    placed all necessary elements for the general layout of questionnaire page
    content not necessary; just placed the following elements to create general layout: quit survey button, progress bar placeholder, question heading, question body, face-cam placeholder, user input widgets (radio buttons for this placeholder), previous question button, next question button and submit survey button.

--- client/src/pages/QuestionnairePage.jsx
+++ client/src/pages/QuestionnairePage.jsx
@@ -1,14 +1,62 @@
 import {Link} from 'react-router-dom';
+import './QuestionnairePage.css';
 
 function QuestionnairePage() {
   return (
-    <div>
-        <h1>Questionnaire</h1>
-        <Link to="/completion">
+    <div id="survey_superdiv">
+
+        <div className="left-aligned">
+          <Link to="/">
+              <button>
+                &lt; Quit Survey
+              </button>
+          </Link>
+        </div>
+
+        <div className="center-aligned"> -----progress bar here-----</div>
+
+        <div className="left-aligned heading"> Question 1: Anxiety</div>
+        <div className="left-aligned subheading"> Over the last two weeks, have you felt nervous, anxious, or on edge? </div>
+
+        <div className="center-aligned flex-container">
+          <div className="face-cam">face-cam</div>
+          <div className="input-widgets">
+            <input type="radio" id="not_at_all" name="answer" value="Not at all"></input>
+            <label for="not_at_all">Not at all</label><br></br>
+
+            <input type="radio" id="rarely" name="answer" value="Rarely"></input>
+            <label for="rarely">Rarely</label><br></br>
+
+            <input type="radio" id="sometimes" name="answer" value="Sometimes"></input>
+            <label for="sometimes">Sometimes</label><br></br>
+
+            <input type="radio" id="most_of_the_time" name="answer" value="Most of the time"></input>
+            <label for="most_of_the_time">Most of the time</label><br></br>
+
+            <input type="radio" id="all_the_time" name="answer" value="All the time"></input>
+            <label for="all_the_time">All the time</label><br></br>
+          </div>
+        </div>
+
+          
+        
+
+        <div className="center-aligned flex-container">
+          <button>
+              &lt; Previous
+          </button>
+
+          <Link to="/completion">
             <button>
-                to completion page
+                Submit Survey
             </button>
-        </Link>
+          </Link>
+
+          <button>
+              Next &gt;
+          </button>
+        </div>
+
     </div>
   );
 }
