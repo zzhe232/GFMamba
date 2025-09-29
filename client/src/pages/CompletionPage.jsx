84fdae08a2002e5de70fcd46f8ca881de62ff65a
YanHmueAung <Hmue_Aung_YAN_from.TP@mpa.gov.sg>
2025-09-30 00:08:27 +1300

    Completion Page

--- client/src/pages/CompletionPage.jsx
+++ client/src/pages/CompletionPage.jsx
@@ -1,14 +1,24 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './CompletionPage.css';
 
 function CompletionPage() {
   return (
-    <div>
-        <h1>Completion Page</h1>
-        <Link to="/">
-            <button>
-                go home
-            </button>
+    <div className="completion-page">
+      <div className="completion-container">
+        <h1 className="completion-title">Thank you for answering the survey.</h1>
+        
+        <p className="completion-message">
+          Thank you for partaking in this study. We highly appreciate the time and effort you allotted to help us meet 
+          our objectives. Rest assured, the collected data will be strictly utilized according to the specifications in 
+          the consent form.
+        </p>
+        
+        <Link to="/" className="home-link">
+          <button className="home-button">
+            Go back to Home Page &gt;&gt;
+          </button>
         </Link>
+      </div>
     </div>
   );
 }
